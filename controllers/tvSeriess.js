749f5988f185dcd9d75962d09bf1110d03d410cd
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-01-17 16:32:48 +0000

    adding to tv wishlist from api search bar working

--- controllers/tvSeriess.js
+++ controllers/tvSeriess.js
@@ -50,9 +50,60 @@ function tvSeriessCreate(req, res, next) {
     });
 }
 
+// EDIT
+function tvSeriessEdit(req, res, next) {
+  TvSeries
+    .findById(req.params.id)
+    .exec()
+    .then((tvSeries) => {
+      if(!tvSeries) return res.notFound();
+      res.render('tvSeriess/edit', { tvSeries });
+    })
+    .catch(next);
+}
+
+// UPDATE
+function tvSeriessUpdate(req, res, next) {
+  console.log(req.body);
+  TvSeries
+    .findById(req.params.id)
+    .exec()
+    .then((tvSeries) => {
+      if(!tvSeries) return res.notFound();
+
+      tvSeries = Object.assign(tvSeries, req.body);
+
+      return tvSeries.save();
+    })
+    .then((tvSeries) => res.redirect(`/tvSeriess/${tvSeries.id}`))
+    .catch((err) => {
+      if(err.name === 'ValidationError') {
+        return res.badRequest(`/tvSeriess/${req.params.id}/edit`, err.toString());
+      }
+      next(err);
+    });
+}
+
+// DELETE
+function tvSeriessDelete(req, res, next) {
+  TvSeries
+    .findById(req.params.id)
+    .exec()
+    .then((tvSeries) => {
+      if(!tvSeries) return res.status(404).send('Not found');
+
+      return tvSeries.remove();
+    })
+    .then(() => res.redirect('/tvSeriess'))
+    .catch(next);
+}
+
 module.exports = {
   index: tvSeriessIndex,
   new: tvSeriessNew,
   show: tvSeriessShow,
-  create: tvSeriessCreate
+  create: tvSeriessCreate,
+  edit: tvSeriessEdit,
+  update: tvSeriessUpdate,
+  delete: tvSeriessDelete
 };
