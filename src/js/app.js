35d4a66b8187f14ad15ce3eba485c7a15013c07b
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-01-18 18:23:11 +0000

    film slick carousel working

--- src/js/app.js
+++ src/js/app.js
@@ -12,10 +12,22 @@ $(() => {
 function addFilm(film) {
   if(film.poster_path) {
     $('.api-container').append(`
-      <div class="slick-item-container">
-        <img class="api-image" src="https://image.tmdb.org/t/p/w500${film.poster_path}">
-      </div>
-  `);
+                              <div class="slick-item-container">
+                                <div class="hovereffect">
+                                  <img src="https://image.tmdb.org/t/p/w500${film.poster_path}">
+                                  <div class="overlay">
+                                    <form method="POST" action="/films">
+                                      <input type="hidden" name="title" value="${film.title}">
+                                      <input type="hidden" name="image" value="${film.poster_path}">
+                                      <input type="hidden" name="synopsis" value="${film.overview}">
+                                      <input type="hidden" name="averageRating" value="${film.vote_average}">
+                                      <input type="hidden" name="year" value="${film.release_date.split('-')[0]}">
+                                        <button class="add-wishlist-button">Add to wishlist</button>
+                                    </form>
+                                  </div>
+                                </div>
+                              </div>
+                          `);
   }
 }
 
@@ -23,9 +35,8 @@ function getFilms(e) {
   e.preventDefault();
   const searchInput = $('input[name="q"]').val();
 
-  if(!$(this).is('button')) {
-    emptyFilms();
-  }
+  if($('.slick-active').length !== 0) $('.api-container').slick('unslick');
+  if(!$(this).is('button')) emptyFilms();
 
   $
     .get(`https://api.themoviedb.org/3/search/movie?api_key=6d36f11289998f020d28569c11326227&query=${searchInput}`)
@@ -33,11 +44,10 @@ function getFilms(e) {
       $.each(films.results, (i, film) => {
         addFilm(film);
       });
-
       $('.api-container').slick({
         infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToShow: 5,
+        slidesToScroll: 5
       });
     });
 }
@@ -50,7 +60,7 @@ function emptyFilms() {
 // --- TV SERIES API SEARCH --- //
 function addTvSeries(tvSeries) {
   $('.api-container').append(`
-                              <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                              <div class="slick-item-container">
                                 <div class="hovereffect">
                                   <img class="api-image" src="https://image.tmdb.org/t/p/w500${tvSeries.poster_path}">
                                     <div class="overlay">
@@ -58,7 +68,7 @@ function addTvSeries(tvSeries) {
                                         <input type="hidden" name="name" value="${tvSeries.name}">
                                         <input type="hidden" name="image" value="${tvSeries.poster_path}">
                                         <input type="hidden" name="averageRating" value="${tvSeries.vote_average}">
-                                      <input class="search-button" type="submit" value="Add to watchlist">
+                                      <input class="search-button" type="submit" value="Add to wishlist">
                                       </form>
                                     </div>
                                   </div>
@@ -71,15 +81,19 @@ function getTvSeries(e) {
   e.preventDefault();
   const searchInput = $('input[name="q"]').val();
 
-  if(!$(this).is('button')) {
-    emptyTvSeries();
-  }
+  if($('.slick-active').length !== 0) $('.api-container').slick('unslick');
+  if(!$(this).is('button')) emptyTvSeries();
 
   $
     .get(`https://api.themoviedb.org/3/search/tv?api_key=6d36f11289998f020d28569c11326227&query=${searchInput}`)
     .then(tvSeriess => {
       $.each(tvSeriess.results, (i, tvSeries) => {
         addTvSeries(tvSeries);
+      });
+      $('.api-container').slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 5
       });
     });
 }
